8c4d91aaa57b5c74b46e29c8167f33e3d734c632
Verdraks <132646355+Verdraks@users.noreply.github.com>
2026-01-12 09:11:56 +0100

    69 fix issue branch debug (#70)

--- Assets/App/Scripts/PoolSystem/PooledObject.cs
+++ Assets/App/Scripts/PoolSystem/PooledObject.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class PooledObject : MonoBehaviour
 {
     private int m_PoolKey;
+    private bool m_Initialized = false;
 
     public void Initialize(int poolKey)
     {
         m_PoolKey = poolKey;
+        m_Initialized = true;
     }
 
     public void Release()
     {
+        if (!m_Initialized) return;
+        m_Initialized = false;
         PoolManager.Instance.ReturnToPool(this.gameObject, m_PoolKey);
     }
 }
