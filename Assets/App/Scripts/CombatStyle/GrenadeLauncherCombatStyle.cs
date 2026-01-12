2f23f71d62cffff135acf14b6ab3630d3374bc6d
Steamcollect <eliot.garnier.080805@gmail.com>
2026-01-12 13:39:13 +0100

    wip

--- Assets/App/Scripts/CombatStyle/GrenadeLauncherCombatStyle.cs
+++ Assets/App/Scripts/CombatStyle/GrenadeLauncherCombatStyle.cs
@@ -73,15 +73,18 @@ public class GrenadeLauncherCombatStyle : CombatStyle
         Vector3 s = m_AttackPoint.position;
         Vector3 e = m_AimTarget.Get().position;
 
-        for (int i = 0; i < m_PreShowLinePointsCount; i++)
+        m_PreShowLine.SetPosition(0, s);
+
+        for (int i = 0; i < m_PreShowLinePointsCount - 2; i++)
         {
             float t = (float)i / (float)m_PreShowLinePointsCount;
-            m_PreShowLine.SetPosition(i, new Vector3(
+            m_PreShowLine.SetPosition(i + 1, new Vector3(
                 Mathf.Lerp(s.x, e.x, t),
                 Mathf.Lerp(s.y, e.y, t) + m_GrenadePrefab.GetMovementCurve().Evaluate(t) * m_GrenadePrefab.GetMinHeight(),
                 Mathf.Lerp(s.z, e.z, t)));
         }
 
+        m_PreShowLine.SetPosition(m_PreShowLinePointsCount - 1, e);
         m_PreShowCylinder.transform.position = e;
     }
 }
