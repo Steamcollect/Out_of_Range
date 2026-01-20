2da23f0a1e56b8bff760bb90d56ca26d631de3a6
Steamcollect <eliot.garnier.080805@gmail.com>
2026-01-20 10:33:49 +0100

    SetActivePatern

--- Assets/App/Scripts/CombatStyle/Laser.cs
+++ Assets/App/Scripts/CombatStyle/Laser.cs
@@ -3,19 +3,12 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     [SerializeField] int m_Damage;
-    [SerializeField] float m_KnockbackForce;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.TryGetComponent(out HurtBox hurtBox))
         {
             hurtBox.TakeDamage(m_Damage);
-
-            if (collision.collider.transform.parent.CompareTag("Player")
-                && collision.collider.transform.parent.TryGetComponent(out Rigidbody rb))
-            {
-                rb.AddForce(collision.contacts[0].normal * m_KnockbackForce, ForceMode.Impulse);
-            }
         }
     }
 }
