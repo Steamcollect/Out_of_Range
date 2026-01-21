938ee07f89d9d70f559d3877437315bfed0ddd47
Baptiste PERRIN <b_perrin6@stu-bellecour.fr>
2026-01-21 12:18:57 +0100

    Prefab updates

--- Assets/App/Scripts/PowerUp/PowerUpPickUp.cs
+++ Assets/App/Scripts/PowerUp/PowerUpPickUp.cs
@@ -5,12 +5,18 @@ public class PowerUpPickUp : MonoBehaviour
 {
     [SerializeField, Inline] PowerUp m_PowerUp;
     [SerializeField] RSO_CurrentPowerUp m_PlayerPowerUp;
+    [SerializeField] private bool m_DestroyAfterPickup = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             m_PlayerPowerUp.Set(m_PowerUp);
+
+            if (m_DestroyAfterPickup)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
