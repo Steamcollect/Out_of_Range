bbc56504a23d7a200a0583f3733be520c3069230
Steamcollect <eliot.garnier.080805@gmail.com>
2026-01-22 21:20:17 +0100

    restart from scratch

--- Assets/App/Scripts/Entitys/Movement/Entity_HorizontalLinearMovement.cs
+++ Assets/App/Scripts/Entitys/Movement/Entity_HorizontalLinearMovement.cs
@@ -1,9 +1,14 @@
+using MoreMountains.Tools;
 using UnityEngine;
 
 public class Entity_HorizontalLinearMovement : MonoBehaviour, IMovement
 {
     [Header("Settings")]
     [SerializeField] private float m_MoveSpeed;
+    
+    [Space(10)]
+    [SerializeField] LayerMask m_GroundLayer;
+    [SerializeField] float m_MaxFallHeight;
 
     [Header("References")]
     [SerializeField] private Rigidbody m_Rb;
@@ -13,7 +18,36 @@ public class Entity_HorizontalLinearMovement : MonoBehaviour, IMovement
 
     public virtual void Move(Vector3 input)
     {
-        m_Rb.AddForce(input * m_MoveSpeed);
+        float stepHeight = 0.4f;
+        float stepDistance = m_MoveSpeed * Time.fixedDeltaTime;
+
+        Vector3 stepCheckPos = transform.position
+                               + Vector3.up * stepHeight
+                               + input * stepDistance;
+
+        bool stepHasGround = Physics.Raycast(
+            stepCheckPos,
+            Vector3.down,
+            out RaycastHit stepHit,
+            stepHeight + m_MaxFallHeight,
+            m_GroundLayer
+        );
+
+        if (stepHasGround)
+        {
+            m_Rb.AddForce(input * m_MoveSpeed);
+            return;
+        }
+
+        Vector3 vel = m_Rb.linearVelocity;
+        float dot = Vector3.Dot(vel, input);
+
+        if (dot > 0f)
+        {
+            vel -= input * dot;
+            m_Rb.linearVelocity = vel;
+        }
+        return;
     }
 
     public void ResetVelocity()
---
+++ Assets/App/Scripts/Entitys/Movement/Player_Dash.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Player_Dash : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float m_DashDistance;
+    [SerializeField] float m_DashForce;
+
+    public void Dash(Vector3 input)
+    {
+
+    }
+}
