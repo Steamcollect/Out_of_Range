7a72f537f374d002549e52dcb2cdca18b2455102
Baptiste PERRIN <b_perrin6@stu-bellecour.fr>
2026-02-02 11:57:23 +0100

    WIP LD

--- Assets/App/Scripts/PowerUp/PowerUpPickUp.cs
+++ Assets/App/Scripts/PowerUp/PowerUpPickUp.cs
@@ -1,5 +1,6 @@
 using MVsToolkit.Dev;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 public class PowerUpPickUp : MonoBehaviour
@@ -7,6 +8,7 @@ public class PowerUpPickUp : MonoBehaviour
     [SerializeField] SSO_PowerUp m_PowerUp;
     [SerializeField] RSO_CurrentPowerUp m_PlayerPowerUp;
     [SerializeField] private bool m_DestroyAfterPickup = false;
+    public UnityEvent onWeaponPickedUp;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +17,7 @@ public class PowerUpPickUp : MonoBehaviour
             if (!m_PowerUp) Debug.LogWarning($"{gameObject.name} : No PowerUp Data assigned!");
             m_PlayerPowerUp.Value.Add(m_PowerUp);
             m_PlayerPowerUp.Set(m_PlayerPowerUp.Value);
+            onWeaponPickedUp?.Invoke();
 
             if (m_DestroyAfterPickup)
             {
