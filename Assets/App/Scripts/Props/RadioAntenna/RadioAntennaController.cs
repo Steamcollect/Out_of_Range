3dc45bbb3417b55ca1b11e5fb32a31d04a6984b1
Tristan Poveda <povedatristan@gmail.com>
2026-02-04 15:16:49 +0100

    Antenne Visual

--- Assets/App/Scripts/Props/RadioAntenna/RadioAntennaController.cs
+++ Assets/App/Scripts/Props/RadioAntenna/RadioAntennaController.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Serialization;
+using UnityEngine.VFX;
 
 public class RadioAntennaController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private RadioAntennaTrigger m_Trigger;
+    [ColorUsage(true, true)] [SerializeField] private Color m_DefaultColor;
+    [ColorUsage(true, true)] [SerializeField] private Color m_ActiveColor;
+    [SerializeField] private VisualEffect m_Effect;
 
     [Header("Output")]
     [SerializeField] private UnityEvent m_OnPlayerInteract;
@@ -16,6 +20,7 @@ public class RadioAntennaController : MonoBehaviour
     {
         m_HasInteract = false;
         m_Trigger.SetCanPlayerInteract(true);
+        m_Effect.SetVector4("Color", m_DefaultColor);
     }
 
     private void OnEnable()
@@ -35,5 +40,6 @@ public class RadioAntennaController : MonoBehaviour
         m_HasInteract = true;
         m_Trigger.SetCanPlayerInteract(false);
         m_OnPlayerInteract.Invoke();
+        m_Effect.SetVector4("Color", m_ActiveColor);
     }
 }
