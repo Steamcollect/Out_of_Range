f2bfa93697ff1d9d29a0300c4187eaf3ace04c70
Verdraks <martinezorlando955@gmail.com>
2026-02-17 11:11:42 +0100

    Fix: CameraTargetVisual only on gamepad

--- Assets/App/Scripts/Camera/CameraTargetVisual.cs
+++ Assets/App/Scripts/Camera/CameraTargetVisual.cs
@@ -8,10 +8,11 @@ public class CameraTargetVisual : MonoBehaviour
     
     [Header("References")]
     [SerializeField] private GameObject m_VisualContainer;
+    [SerializeField] private RSO_CurrentInputDeviceType m_CurrentInputDeviceType;
 
     public void HandleCameraTarget(ITargetable target)
     {
-        m_VisualContainer.SetActive(target != null);
+        m_VisualContainer.SetActive(target != null && m_CurrentInputDeviceType.Get() == InputDeviceType.Gamepad);
         if (target != null)
         {
             m_VisualContainer.transform.position = Vector3.up * (Mathf.Sin(Time.time * m_Frequency) * m_Amplitude) + target.GetTargetIndicatorPosition();
