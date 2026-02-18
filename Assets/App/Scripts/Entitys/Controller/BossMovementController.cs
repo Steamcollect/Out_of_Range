f2b83e1f872e4d56e54eda0838a9cd59a5213a67
Steamcollect <eliot.garnier.080805@gmail.com>
2026-02-18 20:12:33 +0100

    wip

--- Assets/App/Scripts/Entitys/Controller/BossMovementController.cs
+++ Assets/App/Scripts/Entitys/Controller/BossMovementController.cs
@@ -1,15 +1,16 @@
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 
 public class BossMovementController : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] float m_VerticalSpeed;
-    [SerializeField] float m_HorizontalSpeed;
+    [SerializeField] float m_MoveSpeed;
 
     [Space(10)]
     [SerializeField, Tooltip("Center of the rotation, the body will move around it")] Vector3 m_PivotPos;
     [SerializeField] float m_Distance;
+    [SerializeField] float m_VerticalSpeedRatio = .2f;
 
     float m_PosY;
     public Vector2 m_Input;
@@ -24,27 +25,29 @@ public class BossMovementController : MonoBehaviour
     {
         m_PivotPoint = new GameObject("BossPivotPoint").transform;
         m_PivotPoint.position = m_PivotPos;
+
+        m_PosY = transform.position.y;
     }
 
     private void Update()
     {
-        m_PivotPoint.eulerAngles += Vector3.up * m_Input.x * m_HorizontalSpeed * Time.deltaTime;
-        Vector3 targetPos = m_PivotPoint.position + m_PivotPoint.forward * m_Distance;
-
-        m_PosY += m_Input.y * m_VerticalSpeed * Time.deltaTime;
-        targetPos.y = m_PosY;
-
-        transform.position = targetPos;
+        transform.position = GetPosFromInput(transform.position, m_Input * m_MoveSpeed);
 
         Vector3 lookAtPos = m_PivotPoint.position;
         lookAtPos.y = m_PosY;
         transform.LookAt(lookAtPos);
     }
 
-    //public Vector3 GetPosFromInput(Vector3 currentPos, Vector2 input)
-    //{
-        
-    //}
+    public Vector3 GetPosFromInput(Vector3 current, Vector2 direction)
+    {
+        m_PivotPoint.eulerAngles += Vector3.up * direction.x * Time.deltaTime;
+        Vector3 targetPos = m_PivotPoint.position + m_PivotPoint.forward * m_Distance;
+
+        m_PosY += direction.y * m_VerticalSpeedRatio * Time.deltaTime;
+        targetPos.y = m_PosY;
+
+        return targetPos;
+    }
 
     private void OnDrawGizmos()
     {
