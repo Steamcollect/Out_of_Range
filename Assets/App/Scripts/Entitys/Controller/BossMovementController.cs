5166e43c18b299859e250a98bcd831dded10ed67
Steamcollect <eliot.garnier.080805@gmail.com>
2026-02-18 20:03:00 +0100

    Wip

--- Assets/App/Scripts/Entitys/Controller/BossMovementController.cs
+++ Assets/App/Scripts/Entitys/Controller/BossMovementController.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.ProBuilder;
 
 public class BossMovementController : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] float m_MoveSpeed;
+    [SerializeField] float m_VerticalSpeed;
+    [SerializeField] float m_HorizontalSpeed;
 
+    [Space(10)]
     [SerializeField, Tooltip("Center of the rotation, the body will move around it")] Vector3 m_PivotPos;
     [SerializeField] float m_Distance;
 
+    float m_PosY;
     public Vector2 m_Input;
 
     //[Header("References")]
@@ -24,10 +28,23 @@ public class BossMovementController : MonoBehaviour
 
     private void Update()
     {
-        m_PivotPoint.eulerAngles += new Vector3(m_Input.y, m_Input.x, 0) * m_MoveSpeed * Time.deltaTime;
-        transform.position = m_PivotPoint.position + m_PivotPoint.forward * m_Distance;
-        transform.LookAt(m_PivotPoint.position);
+        m_PivotPoint.eulerAngles += Vector3.up * m_Input.x * m_HorizontalSpeed * Time.deltaTime;
+        Vector3 targetPos = m_PivotPoint.position + m_PivotPoint.forward * m_Distance;
+
+        m_PosY += m_Input.y * m_VerticalSpeed * Time.deltaTime;
+        targetPos.y = m_PosY;
+
+        transform.position = targetPos;
+
+        Vector3 lookAtPos = m_PivotPoint.position;
+        lookAtPos.y = m_PosY;
+        transform.LookAt(lookAtPos);
     }
+
+    //public Vector3 GetPosFromInput(Vector3 currentPos, Vector2 input)
+    //{
+        
+    //}
 
     private void OnDrawGizmos()
     {
