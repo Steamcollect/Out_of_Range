427fed817dd9f15bba815c1e0418cc444997d737
Steamcollect <eliot.garnier.080805@gmail.com>
2026-02-18 11:05:08 +0100

    Update streaks

--- Assets/App/Scripts/Entitys/Player/PlayerKillStreakManager.cs
+++ Assets/App/Scripts/Entitys/Player/PlayerKillStreakManager.cs
@@ -1,27 +1,20 @@
 using System.Linq;
-using MoreMountains.Feedbacks;
-using MVsToolkit.Dev;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class PlayerKillStreakManager : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField, ReadOnly] int m_CurrentStreak = 0;
+    [SerializeField] float m_StepTime = 8;
     [SerializeField] KillStreakStep[] m_Steps;
 
+    [SerializeField] string m_CombosNames;
+
+    [Space(5)]
+    [SerializeField, ReadOnly] int m_CurrentStreak = 0;
+    
     float m_Timer;
     int m_CurrentStep;
-
-    [System.Serializable]
-    struct KillStreakStep
-    {
-        public int KillStreakRequire;
-        public float SpeedMult;
-
-        [Space(10)]
-        public string StepName;
-        public Color StepColor;
-    }
 
     [Header("References")]
     [SerializeField] RSO_PlayerController m_Player;
@@ -47,12 +40,34 @@ public class PlayerKillStreakManager : MonoBehaviour
     private void Start()
     {
         m_Steps = m_Steps.OrderBy(c => c.KillStreakRequire).ToArray();
-        if (m_Steps[0].KillStreakRequire > 0)
-        {
 
+        if (m_Steps.Length > 0 && m_Steps[0].KillStreakRequire > 0)
+        {
+            KillStreakStep baseStep = new KillStreakStep();
+
+            KillStreakStep[] newArray = new KillStreakStep[m_Steps.Length + 1];
+            newArray[0] = baseStep;
+            for (int i = 0; i < m_Steps.Length; i++)
+                newArray[i + 1] = m_Steps[i];
+
+            m_Steps = newArray;
         }
 
         m_Player.Value.GetMovement().SetSpeedMult(1);
+    }
+
+    private void Update()
+    {
+        if(m_CurrentStep > 0)
+        {
+            m_Timer -= Time.deltaTime;
+
+            if(m_Timer <= 0)
+            {
+                m_CurrentStep--;
+                m_Timer = m_StepTime;
+            }
+        }
     }
 
     void OnEnemyKilled()
@@ -67,7 +82,6 @@ public class PlayerKillStreakManager : MonoBehaviour
                 m_Player.Value.GetMovement().SetSpeedMult(m_Steps[i].SpeedMult);
             }
         }
-
     }
 
     void OnPlayerTakeDamage()
@@ -75,5 +89,80 @@ public class PlayerKillStreakManager : MonoBehaviour
         m_CurrentStreak = 0;
         m_CurrentStep = 0;
         m_Player.Value.GetMovement().SetSpeedMult(1);
+    }
+
+    [Button]
+    void LerpSteps()
+    {
+        if (m_Steps == null || m_Steps.Length < 2)
+            return;
+
+        KillStreakStep first = m_Steps[0];
+        KillStreakStep last = m_Steps[m_Steps.Length - 1];
+
+        int count = m_Steps.Length;
+
+        for (int i = 0; i < count; i++)
+        {
+            float t = (float)i / (count - 1);
+            m_Steps[i].SpeedMult = Mathf.Lerp(first.SpeedMult, last.SpeedMult, t);
+        }
+    }
+
+    [Button]
+    void CreateStepsByName()
+    {
+        if (string.IsNullOrWhiteSpace(m_CombosNames))
+            return;
+
+        // Sépare par virgule et nettoie les espaces
+        string[] names = m_CombosNames
+            .Split(',')
+            .Select(n => n.Trim())
+            .Where(n => !string.IsNullOrEmpty(n))
+            .ToArray();
+
+        // Crée un tableau de steps correspondant
+        m_Steps = new KillStreakStep[names.Length];
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            m_Steps[i] = new KillStreakStep
+            {
+                StepName = names[i],
+                SpeedMult = 1f,          // Valeur par défaut
+                StepColor = Color.white  // Valeur par défaut
+            };
+        }
+    }
+
+
+    public KillStreakStep GetKillStreakStep() =>m_Steps[m_CurrentStep];
+
+    private void OnValidate()
+    {
+        for (int i = 0; i < m_Steps.Length; i++)
+        {
+            m_Steps[i].KillStreakRequire = i;
+        }
+    }
+}
+
+[System.Serializable]
+public class KillStreakStep
+{
+    public float SpeedMult;
+    [ReadOnly] public int KillStreakRequire;
+
+    [Space(10)]
+    public string StepName;
+    public Color StepColor;
+
+    public KillStreakStep()
+    {
+        KillStreakRequire = 0;
+        SpeedMult = 1;
+        StepName = "";
+        StepColor = Color.white;
     }
 }
