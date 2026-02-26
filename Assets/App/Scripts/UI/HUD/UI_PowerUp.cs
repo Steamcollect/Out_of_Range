9a4859fd3da4683de3ca7f20ae929bb6f15c3a7e
Steamcollect <107922742+Steamcollect@users.noreply.github.com>
2026-02-26 12:05:13 +0100

    Rework powerUp HUD (#170)

--- Assets/App/Scripts/UI/HUD/UI_PowerUp.cs
+++ Assets/App/Scripts/UI/HUD/UI_PowerUp.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,7 +7,11 @@ public class UI_PowerUp : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] UI_PowerUpSlot m_PowerUpSlotPrefab;
-        
+
+    [Header("Animation Settings")]
+    [SerializeField] float m_DelayBetweenWaves;
+    [SerializeField] float m_DelayBetweenElements;
+
     [Header("Input")]
     [SerializeField] RSE_OnPlayerPowerUpChange m_OnPowerUpChange;
 
@@ -14,6 +19,11 @@ public class UI_PowerUp : MonoBehaviour
         
     private void OnEnable() => m_OnPowerUpChange.Action += UpdateUI;
     private void OnDisable() => m_OnPowerUpChange.Action -= UpdateUI;
+
+    private void Start()
+    {
+        StartCoroutine(Animation());
+    }
 
     private void UpdateUI(List<PowerUpHandler> powerUps)
     {
@@ -42,7 +52,21 @@ public class UI_PowerUp : MonoBehaviour
         foreach (UI_PowerUpSlot slot in toRemove)
         {
             m_PowerUpSlots.Remove(slot);
-            Destroy(slot.gameObject);
+            slot.DestoyUI();
         }
+    }
+
+    IEnumerator Animation()
+    {
+        yield return new WaitUntil(() => m_PowerUpSlots.Count > 0);
+
+        foreach (var item in m_PowerUpSlots)
+        {
+            item.Bump();
+            yield return new WaitForSeconds(m_DelayBetweenElements);
+        }
+
+        yield return new WaitForSeconds(m_DelayBetweenWaves);
+        StartCoroutine(Animation());
     }
 }
