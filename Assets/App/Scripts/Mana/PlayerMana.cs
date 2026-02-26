6b70df402d7fd45f2e1b0731a594fd1a8ca943f0
Steamcollect <eliot.garnier.080805@gmail.com>
2026-02-26 20:30:29 +0100

    Add mana over time

--- Assets/App/Scripts/Mana/PlayerMana.cs
+++ Assets/App/Scripts/Mana/PlayerMana.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using MVsToolkit.Dev;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,8 +9,11 @@ public class PlayerMana : MonoBehaviour
     [Header("References")]
     [SerializeField] private int m_MaxMana = 100;
     [SerializeField, ReadOnly] int m_CurrentMana;
-        
+
+    [SerializeField] int m_ManaGivenPerSec = 1;
+
     public event Action OnManaChanged;
+    public event Action OnManaAddedOverTime;
     public UnityEvent OnManaCollected;
     
     public int CurrentMana => m_CurrentMana;
@@ -18,6 +22,19 @@ public class PlayerMana : MonoBehaviour
     private void Start()
     {
         SetToMax();
+        StartCoroutine(ManaOverTime());
+    }
+
+    IEnumerator ManaOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(() => m_CurrentMana < m_MaxMana);
+
+            yield return new WaitForSeconds(1);
+            m_CurrentMana = Math.Clamp(m_CurrentMana + m_ManaGivenPerSec, 0, m_MaxMana);
+            OnManaAddedOverTime.Invoke();
+        }
     }
 
     public void Add(int amount)
