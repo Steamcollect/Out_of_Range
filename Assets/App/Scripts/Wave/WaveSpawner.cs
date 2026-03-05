800917db5f769bd4488e4e83203797eb938baaa1
Steamcollect <eliot.garnier.080805@gmail.com>
2026-03-05 17:38:03 +0100

    BuildFix

--- Assets/App/Scripts/Wave/WaveSpawner.cs
+++ Assets/App/Scripts/Wave/WaveSpawner.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
 
 public class WaveSpawner : MonoBehaviour
 {
