13efdd017bc9c3833d5dabfa271372105ecfd516
Tristan Poveda <povedatristan@gmail.com>
2026-03-09 17:14:42 +0100

    level art + glitch shader

---
+++ Assets/App/Arts/Shaders/Effects/GlitchEffect.shader
@@ -0,0 +1,52 @@
+﻿Shader "Custom/GlitchEffect"
+{
+    SubShader
+    {
+        Tags { "RenderPipeline"="UniversalPipeline" "RenderType"="Opaque" }
+
+        Pass
+        {
+            Name "GlitchEffect"
+
+            Blend SrcAlpha OneMinusSrcAlpha
+            ZWrite Off
+            ZTest  Always
+            Cull Off
+
+            HLSLPROGRAM
+            #pragma vertex Vert
+            #pragma fragment Fragment
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+            float2 _ScanLineJitter;
+            float  _HorizontalShake;
+            float2 _ColorDrift;
+
+            float nrand(float x, float y)
+            { return frac(sin(dot(float2(x, y), float2(12.9898, 78.233))) * 43758.5453); }
+
+            half4 Fragment(Varyings i) : SV_Target
+            {
+                float u = i.texcoord.x;
+                float v = i.texcoord.y;
+
+                float jitter = (nrand(v, _Time.x) * 2 - 1);
+                jitter *= step(_ScanLineJitter.y, abs(jitter)) * _ScanLineJitter.x;
+                float shake = (nrand(_Time.x, 2) - 0.5) * _HorizontalShake;
+                float drift = sin(v + _ColorDrift.y) * _ColorDrift.x;
+
+                float2 uv1 = frac(float2(u + jitter + shake,           v));
+                float2 uv2 = frac(float2(u + jitter + shake + drift,    v));
+
+                half4 s1 = SAMPLE_TEXTURE2D(_BlitTexture, sampler_LinearRepeat, uv1);
+                half4 s2 = SAMPLE_TEXTURE2D(_BlitTexture, sampler_LinearRepeat, uv2);
+
+                half4 rgba = half4(s1.r, s2.g, s1.b, 1.0) * max(s1.a, s2.a);
+
+                return rgba;
+            }
+            ENDHLSL
+        }
+    }
+}
---
+++ Assets/App/Scripts/Effetcs/GlitchEffectFeature.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.RenderGraphModule.Util;
+
+namespace GlitchEffect
+{
+    public class GlitchEffectFeature : ScriptableRendererFeature
+    {
+        class GlitchEffectPass : ScriptableRenderPass
+        {
+            private Material material;
+            private LayerMask layerMask;
+
+            private List<ShaderTagId> shaderTag = new List<ShaderTagId>();
+
+            static readonly int ScanLineJitterID = Shader.PropertyToID("_ScanLineJitter");
+            static readonly int HorizontalShakeID = Shader.PropertyToID("_HorizontalShake");
+            static readonly int ColorDriftID = Shader.PropertyToID("_ColorDrift");
+            public GlitchEffectPass(Shader shader, LayerMask layerMask)
+            {
+                if (shader != null) material = CoreUtils.CreateEngineMaterial(shader);
+                this.layerMask = layerMask;
+            }
+            private class PassData
+            {
+                public RendererListHandle rendererListHandle;
+                public TextureHandle texture;
+                public Material material;
+            }
+
+            private void InitRendererLists(ContextContainer frameData, ref PassData passData, RenderGraph renderGraph)
+            {
+                UniversalRenderingData universalRenderingData = frameData.Get<UniversalRenderingData>();
+                UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+                UniversalLightData lightData = frameData.Get<UniversalLightData>();
+
+                var sortFlags = cameraData.defaultOpaqueSortFlags;
+                RenderQueueRange renderQueueRange = RenderQueueRange.all;
+                FilteringSettings filterSettings = new FilteringSettings(renderQueueRange, layerMask);
+
+                ShaderTagId[] forwardOnlyShaderTagIds = new ShaderTagId[]
+                {
+                new ShaderTagId("UniversalForwardOnly"),
+                new ShaderTagId("UniversalForward"),
+                new ShaderTagId("SRPDefaultUnlit"),
+                new ShaderTagId("LightweightForward")
+                };
+
+                shaderTag.Clear();
+
+                foreach (ShaderTagId sid in forwardOnlyShaderTagIds)
+                    shaderTag.Add(sid);
+
+                DrawingSettings drawSettings = RenderingUtils.CreateDrawingSettings(shaderTag, universalRenderingData, cameraData, lightData, sortFlags);
+
+                var param = new RendererListParams(universalRenderingData.cullResults, drawSettings, filterSettings);
+                passData.rendererListHandle = renderGraph.CreateRendererList(param);
+            }
+
+            private void UpdateSettings()
+            {
+                if (material == null) { Debug.LogError("update settings material null"); return; }
+                var _volume = VolumeManager.instance.stack.GetComponent<GlitchEffectVolume>();
+
+                var scanLineJitter = _volume.scanLineJitter.value;
+                var horizontalShake = _volume.horizontalShake.value;
+                var colorDrift = _volume.colorDrift.value;
+
+                var slThresh = Mathf.Clamp01(1.0f - scanLineJitter * 1.2f);
+                var slDisp = 0.002f + Mathf.Pow(scanLineJitter, 3) * 0.05f;
+                material.SetVector(ScanLineJitterID, new Vector2(slDisp, slThresh));
+
+                material.SetFloat(HorizontalShakeID, horizontalShake * 0.2f);
+
+                var cd = new Vector2(colorDrift * 0.04f, Time.time * 606.11f);
+                material.SetVector(ColorDriftID, cd);
+            }
+
+            static void ExecutePass(PassData data, RasterGraphContext context)
+            {
+                context.cmd.ClearRenderTarget(false, true, Color.clear);
+                context.cmd.DrawRendererList(data.rendererListHandle);
+            }
+
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+            {
+                var result = frameData.Get<UniversalResourceData>();
+                var camData = frameData.Get<UniversalCameraData>();
+
+                if (!camData.postProcessEnabled || camData.isSceneViewCamera) return;
+                if (result.isActiveTargetBackBuffer) return;
+
+                var desc = renderGraph.GetTextureDesc(result.activeColorTexture);
+                desc.name = "MySpecialRT";
+                desc.clearBuffer = true;
+                desc.colorFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm;
+                var offscreen = renderGraph.CreateTexture(desc);
+
+                UpdateSettings();
+
+                using (var pass = renderGraph.AddRasterRenderPass<PassData>("Special Layer Render", out var passData))
+                {
+                    passData.texture = offscreen;
+                    InitRendererLists(frameData, ref passData, renderGraph);
+
+                    pass.UseRendererList(passData.rendererListHandle);
+                    pass.SetRenderAttachment(offscreen, 0, AccessFlags.Write);
+                    pass.SetRenderAttachmentDepth(result.activeDepthTexture, AccessFlags.Read);
+
+                    pass.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
+                }
+
+                RenderGraphUtils.BlitMaterialParameters param = new(offscreen, result.activeColorTexture, material, 0);
+
+                renderGraph.AddBlitPass(param, "Compose Special Layer");
+            }
+        }
+        [SerializeField] private Shader glitchEffectShader;
+        [SerializeField] private LayerMask layerMask;
+
+        GlitchEffectPass scriptablePass;
+
+        public override void Create()
+        {
+            scriptablePass = new GlitchEffectPass(glitchEffectShader, layerMask);
+            scriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(scriptablePass);
+        }
+    }
+}
