edfa6865b12e94ad4378a7ebb08485df5d0d28fc
Steamcollect <eliot.garnier.080805@gmail.com>
2026-03-11 10:10:54 +0100

    Update debug panel + Credits panel

--- Assets/App/Scripts/Tools/Debug/DebugPanel.cs
+++ Assets/App/Scripts/Tools/Debug/DebugPanel.cs
@@ -5,7 +5,6 @@ public class DebugPanel : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] GameObject m_Content;
-    [SerializeField] GameObject m_ToggleTxt;
 
     [Header("References")]
     [SerializeField] InputActionReference m_ToggleIA;
@@ -17,7 +16,6 @@ public class DebugPanel : MonoBehaviour
     {
         m_ToggleIA.action.Enable();
         m_Content.SetActive(false);
-        m_ToggleTxt.SetActive(!m_Content.activeSelf);
     }
 
     private void OnEnable()
@@ -33,6 +31,5 @@ public class DebugPanel : MonoBehaviour
     void Toggle(InputAction.CallbackContext ctx)
     {
         m_Content.SetActive(!m_Content.activeSelf);
-        m_ToggleTxt.SetActive(!m_Content.activeSelf);
     }
 }
